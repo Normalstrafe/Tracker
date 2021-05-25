69238ac0976b6718a6bcb22e553f78685b61dfda
Normalstrafe <nazarsavjak@gmail.com>
2021-05-25 10:15:16 +0300

    add pages no design

---
+++ app/src/main/java/com/example/tracker/Calculate.java
@@ -0,0 +1,16 @@
+package com.example.tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Calculate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+    }
+}
